12477189995f4ccc134cfef5ce893ca4d5fb2604
Wei Liu <liuwi@pascal-deep-02.cluster.intern>
2023-05-23 14:34:07 +0200

    environment

---
+++ requirements.sh
@@ -0,0 +1,6 @@
+pip3 install torch==1.10.0+cu102 torchvision==0.11.0+cu102 torchaudio==0.10.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip3 install transformers==4.6.0
+pip3 install scikit-learn # 1.0.2 
+pip3 install tqdm # 4.64.0
+pip3 install numpy # 1.22.3
+pip3 install sentencepiece # 0.1.96 
